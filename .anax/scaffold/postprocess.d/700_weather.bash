19c21a538e48c3a9e5e5520739c7be184169c6f1
mangepong <43117908+mangepong@users.noreply.github.com>
2020-08-31 17:23:11 +0200

    Update 700_weather.bash

--- .anax/scaffold/postprocess.d/700_weather.bash
+++ .anax/scaffold/postprocess.d/700_weather.bash
@@ -2,7 +2,7 @@
 #
 # Module scaffold
 
-rsync -av vendor/anax/emil-ramverk1/config ./
-rsync -av vendor/anax/emil-ramverk1/src ./
-rsync -av vendor/anax/emil-ramverk1/view ./
-rsync -av vendor/anax/emil-ramverk1/test ./
+rsync -av vendor/mange/emil-ramverk1/config ./
+rsync -av vendor/mange/emil-ramverk1/src ./
+rsync -av vendor/mange/emil-ramverk1/view ./
+rsync -av vendor/mange/emil-ramverk1/test ./
